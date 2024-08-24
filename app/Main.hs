7e28d6f359b9acde68810353e9821f54d02c4954
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2024-08-24 16:57:37 -0400

    better control flow

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,16 @@ import SDL hiding (get)
 import Control.Monad.Trans
 import Control.Monad.State
 import Control.Concurrent
-import System.Mem
 import Data.Array
 import qualified Data.Set as Set
 
 import Foreign.C.Types
-import GHC.Int
-import Data.Bifunctor
-import Data.Distributive
 import System.Random
 
 type Hrm = StateT (Grid, Texture) IO
+
+data GameFlow = Quit | Continue
+  deriving (Show, Eq)
 
 data TileInfo = Blank CInt | Mine | BlownUp
   deriving (Show, Eq)
@@ -42,9 +41,10 @@ idk :: Num a => (a ->  a) -> V2 a -> V2 a
 idk f (V2 x y) = (V2 (f x) (f y))
 
 
+covered, pressed, flagged, _qmark, _qmarkpressed, mine, blownup, _xmine :: Rectangle CInt
 -- coords in assets sheet
-[covered, pressed, flagged, _qmark, _qmarkpressed, mine, blownup, _xmine ] = 
-    take 8 $ map (\x -> Rectangle (P (V2 (14 + x) 195)) (V2 15 15)) [0,17..]
+covered:pressed:flagged:_qmark:_qmarkpressed:mine:blownup:_xmine:_  = 
+    map (\x -> Rectangle (P (V2 (14 + x) 195)) (V2 15 15)) [0,17..]
 
 numbers :: Array CInt (Rectangle CInt)
 numbers = array (0, 8) $ [(0, pressed)] ++ numbered
@@ -58,7 +58,7 @@ pickTileRect (Tile Mine Shown) = mine
 pickTileRect (Tile BlownUp Shown) = blownup
 pickTileRect (Tile (Blank n) Shown) = numbers ! n
 pickTileRect (Tile _ Flagged) = flagged
-pickTileRect _ = covered
+-- pickTileRect _ = covered
 
 drawTile :: Renderer -> Texture -> (CInt, CInt, Tile) -> IO ()
 drawTile renderer texture (i, j, tile) = do
@@ -73,12 +73,13 @@ renderGrid renderer = do
     mapM_ (liftIO . (drawTile renderer texture))
           [(i, j, grid ! (i, j)) | i <- [0..9], j <- [0..9]]
 
-dispatch :: Renderer -> [EventPayload] -> Hrm ()
-dispatch renderer ((KeyboardEvent ke):_)
+dispatch :: [EventPayload] -> Hrm GameFlow
+dispatch ((KeyboardEvent ke):_)
   | keyboardEventKeyMotion ke == Pressed
-    && keysymKeycode (keyboardEventKeysym ke) == KeycodeQ =
-        liftIO $ putStrLn "quit!"
-dispatch renderer ((MouseButtonEvent me):xs)
+  && keysymKeycode (keyboardEventKeysym ke) == KeycodeQ = do
+        liftIO $ putStrLn "quitting"
+        pure Quit
+dispatch ((MouseButtonEvent me):xs)
   | mouseButtonEventMotion me == Pressed
     && i >= 0 && i <= 9 && j >= 0 && j <= 9 = do
       (grid, texture) <- get
@@ -89,35 +90,43 @@ dispatch renderer ((MouseButtonEvent me):xs)
                  (Tile ti Covered) | mb == ButtonLeft -> Tile ti Shown
                  same -> same
       put (grid // [((i, j), new)], texture)
-      dispatch renderer xs
+      dispatch xs
       where P (V2 x y) = mouseButtonEventPos me
             i = CInt $ x `div` 60
             j = CInt $ y `div` 60
             mb = mouseButtonEventButton me
-dispatch renderer (_:xs) = dispatch renderer xs
-dispatch renderer [] = appLoop renderer
+dispatch (_:xs) = dispatch xs
+dispatch [] = pure Continue
 
 houseKeeping :: Grid -> (Bool, Grid)
 houseKeeping grid
   | lost = (True, grid // 
       [(ix, Tile Mine Shown) | ix <- indices grid, isMine (grid ! ix)])
-  | otherwise = (False, grid)
-  where lost = any (\(Tile ti tv) -> ti == BlownUp) $ elems grid
+  | otherwise = (False, grid // toReveal)
+  where lost = any (\(Tile ti _) -> ti == BlownUp) $ elems grid
+        toReveal = [(ix, makeShown (grid ! ix)) | ix <- indices grid, 1 <= zeros ix]
+        makeShown (Tile ti _) = Tile ti Shown
+        zeros (i, j) =
+            sum [ case (grid ! (i+oi, j+oj)) of
+                        Tile (Blank 0) Shown -> 1
+                        _ -> 0
+                  | oi <- [-1..1], oj <- [-1..1], 
+                  withinGrid (i+oi), withinGrid (j+oj) ]
         isMine (Tile Mine _) = True
         isMine _ = False
 
 appLoop :: Renderer -> Hrm ()
 appLoop renderer = do
   events <- pollEvents
+  flow <- dispatch $ map eventPayload events
   (grid, texture) <- get
   let (lost, grid') = houseKeeping grid
   clear renderer
   put (grid', texture)
   renderGrid renderer
   present renderer
-  if lost
-     then liftIO $ threadDelay 2000000
-     else dispatch renderer $ map eventPayload events
+  if lost then liftIO $ threadDelay 2000000 else pure ()
+  unless (lost || (flow == Quit)) $ appLoop renderer
 
 
 genUnique :: (CInt, CInt) -> Int -> StdGen -> [CInt]
@@ -128,7 +137,11 @@ genUnique interval n gen = helper gen Set.empty
                 let (i, g') = (uniformR interval g) in helper g' $ Set.insert i s
 
 clamp :: Ord a => a -> a -> a -> a
-clamp minVal maxVal x = max minVal (min maxVal x)
+clamp minVal maxVal x = max minVal $ min maxVal x
+
+withinGrid :: CInt -> Bool
+withinGrid n = n == clamp 0 9 n
+
 
 makeGrid :: StdGen -> Grid
 makeGrid g = array ((0, 0), (9, 9)) [((i, j), choose (i, j)) | i <- [0..9], j <- [0..9]]
@@ -137,15 +150,13 @@ makeGrid g = array ((0, 0), (9, 9)) [((i, j), choose (i, j)) | i <- [0..9], j <-
             | otherwise = Tile (Blank (neighbors c)) Covered
           mines = Set.fromList [(i `div` 10, i `mod` 10) 
                                 | i <- genUnique (0, 99) 10 g]
-          clamp' = clamp 0 9
           neighbors (i, j) = 
-              sum [ if Set.member (clamp' i+oi, clamp' j+oj) mines
+              sum [ if Set.member (i+oi, j+oj) mines
                        then 1 else 0
-                    | oi <- [-1..1], oj <- [-1..1] ]
+                | oi <- [-1..1], oj <- [-1..1] ]
 
 main :: IO ()
 main = do
-    putStrLn "Hello, Haskell!"
     initializeAll
     window <- createWindow "My SDL Application" 
                             (defaultWindow { windowInitialSize = V2 600 600 })
